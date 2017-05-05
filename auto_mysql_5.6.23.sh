d0ece4388814e55174d6ac11f9f87135b3c48991
hly <764676555@qq.com>
2017-05-05 15:30:55 +0800

    fix auto_mysql ok

--- auto_mysql_5.6.23.sh
+++ auto_mysql_5.6.23.sh
@@ -1,100 +1,26 @@
 #!/bin/bash
 
 scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. ./mysql_func.sh
 
 port=$1
 mysql_install_dir=$2
-#hostip=$3
 mysql_package_dir=$3
 
 ######################################################################
-Usage (){
-  echo "input wrong..."
-  echo "Usage:./1.sh port mysql_install_dir mysql_package_dir"
-  echo "example ./sh 3317 /data/mysql1 /usr/local/hly/mysql"  
-  exit
-}
-
-check_sock (){
-  let j=0
-  while (( $j == 0 ));do
-    if [[ ! -e "/tmp/mysql_${port}.sock" ]];then
-      echo "no sock,wait 10s"
-      sleep 10
-    else 
-      let j++
-      echo 'sock is exists...'
-    fi
-  done
-}
-
-check_mysql_group_and_user() {
-  egrep "mysql" /etc/group >& /dev/null  
-  test $? != 0 && groupadd mysql 
-
-  egrep "mysql" /etc/passwd >& /dev/null  
-  test $? != 0 && useradd -r -g mysql  mysql 
-}
-
-## 1:red hat 2:ubtun 3:suse
-check_server_version() {
-  version=`cat /proc/version |awk -F "[()]" '{print $5}'`
-  if [[ $version =~ ^[Rr][Ee][Dd].* ]];then 
-    version_type=1
-  elif [[ $version =~ ^[Uu][Bb][Uu].* ]];then 
-    version_type=2
-  else 
-    echo "ERROR not found server version..." && exit 1
-  fi
-}
-
-check_install_dep() {
-  echo "pass"
-}
-
-check_install_dir() {
-  if [[ ! -d ${mysql_install_dir} ]];then 
-    sudo mkdir "${mysql_install_dir}"
-  else
-    echo "${mysql_install_dir} is exist,exit..."
-    exit
-  fi
-}
-
-change_mycnf() {
-  id=`date +%s`
-  sed -i "s:server_id.*=.*:server_id                      = ${id}:g" my.cnf
-  sed -i "s:port.*=.*:port                           = ${port}:g" my.cnf
-  sed -i "s:basedir.*=.*:basedir                        = ${mysql_install_dir}:g" my.cnf
-  sed -i "s:datadir.*=.*:datadir                        = ${mysql_install_dir}/data:g" my.cnf
-  sed -i "s:socket.*=.*:socket                         = /tmp/mysql_${port}.sock:g" my.cnf
-  echo "skip-grant-tables" >> ${mysql_install_dir}/my.cnf
-#sed -i "s///g" my.cnf
-}
 
 if [[ $@ < 2 ]];then
   Usage
 fi
 
 check_mysql_group_and_user
-
 check_install_dir
-
-cd ${mysql_install_dir}
-cp -r $mysql_package_dir/*  ${mysql_install_dir}/
-test $? != 0 && echo "cp mysql_package to install dir failed, exit..." && exit 1
-
-chmod 755 support-files/* scripts/*
-chown -R mysql .
-chgrp -R mysql .
-test $? != 0 && echo "Can't chown or chgrp ,Permission denied, exit..." && exit 1
-
-test ! -e my.cnf && echo "Can't find my.cnf file, exit..." && exit 1
+cp_mysql_file
+install_pre
 change_mycnf
 
-scripts/mysql_install_db --defaults-file=${mysql_install_dir}/my.cnf --user=mysql
-
-bin/mysqld_safe --defaults-file=${mysql_install_dir}/my.cnf &
+scripts/mysql_install_db --defaults-file=${mysql_install_dir}/my.cnf --user=mysql >> /dev/null
+bin/mysqld_safe --defaults-file=${mysql_install_dir}/my.cnf & >> /dev/null
 
 if [[ ! -e "/usr/bin/mysql" ]];then
   ln -s ${mysql_install_dir}/bin/mysql /usr/bin/mysql >> ${scriptsdir}/install_mysql.log 2>&1
@@ -103,13 +29,21 @@ sleep 10s
 
 check_sock
 echo "change mysql password"
-expect ${scripts_dir}/change_pw.exp "root" "${port}"
+expect ${scripts_dir}/change_pw.exp "root" "${port}" >> /dev/null
 sleep 10s
 
 check_sock
 sed -i '/skip-grant-tables/d' ${mysql_install_dir}/my.cnf
-expect ${scripts_dir}/change_pw_ag.exp "root" "${port}"
+expect ${scripts_dir}/change_pw_ag.exp "root" "${port}" >> /dev/null
 
 sleep 5s
 check_sock
-mysql -uaction -paction -h127.0.0.1 -P${port} -e "flush privileges;"
+mysql -uaction -paction -h127.0.0.1 -P${port} -e "show databases;" >> /dev/null
+test $? != 0 && echo "add user to mysql failed, exit..." && exit 1
+
+add_alias=`grep  "\-P${port}" ~/.bashrc|wc -l`
+if [[ $add_alias == 0 ]];then
+  echo "alias mysql${port}='mysql -uaction -paction -P${port} -h 127.0.0.1'" >> ~/.bashrc && source ~/.bashrc 
+fi
+
+echo "install ok....."
---
+++ mysql_func.sh
@@ -0,0 +1,77 @@
+Usage (){
+  echo "input wrong..."
+  echo "Usage:./1.sh port mysql_install_dir mysql_package_dir"
+  echo "example ./sh 3317 /data/mysql1 /usr/local/hly/mysql"  
+  exit
+}
+
+check_sock (){
+  let j=0
+  while (( $j == 0 ));do
+    if [[ ! -e "/tmp/mysql_${port}.sock" ]];then
+      echo "no sock,wait 10s"
+      sleep 10
+    else 
+      let j++
+    fi
+  done
+}
+
+check_mysql_group_and_user() {
+  egrep "mysql" /etc/group >& /dev/null  
+  test $? != 0 && groupadd mysql 
+
+  egrep "mysql" /etc/passwd >& /dev/null  
+  test $? != 0 && useradd -r -g mysql  mysql 
+}
+
+## 1:red hat 2:ubtun 3:suse
+check_server_version() {
+  version=`cat /proc/version |awk -F "[()]" '{print $5}'`
+  if [[ $version =~ ^[Rr][Ee][Dd].* ]];then 
+    version_type=1
+  elif [[ $version =~ ^[Uu][Bb][Uu].* ]];then 
+    version_type=2
+  else 
+    echo "ERROR not found server version..." && exit 1
+  fi
+}
+
+check_install_dep() {
+  echo "pass"
+}
+
+check_install_dir() {
+  if [[ ! -d ${mysql_install_dir} ]];then 
+    sudo mkdir "${mysql_install_dir}"
+  else
+    echo "${mysql_install_dir} is exist,exit..."
+    exit
+  fi
+}
+
+change_mycnf() {
+  id=`date +%s`
+  sed -i "s:server_id.*=.*:server_id                      = ${id}:g" my.cnf
+  sed -i "s:port.*=.*:port                           = ${port}:g" my.cnf
+  sed -i "s:basedir.*=.*:basedir                        = ${mysql_install_dir}:g" my.cnf
+  sed -i "s:datadir.*=.*:datadir                        = ${mysql_install_dir}/data:g" my.cnf
+  sed -i "s:socket.*=.*:socket                         = /tmp/mysql_${port}.sock:g" my.cnf
+  echo "skip-grant-tables" >> ${mysql_install_dir}/my.cnf
+#sed -i "s///g" my.cnf
+}
+
+cp_mysql_file() {
+  echo "cp mysql file to install dir...wait a minute"
+  cd ${mysql_install_dir}
+  cp -r $mysql_package_dir/*  ${mysql_install_dir}/
+  test $? != 0 && echo "cp mysql_package to install dir failed, exit..." && exit 1
+}
+
+install_pre() {
+  chmod 755 support-files/* scripts/*
+  chown -R mysql .
+  chgrp -R mysql .
+  test $? != 0 && echo "Can't chown or chgrp ,Permission denied, exit..." && exit 1
+  test ! -e my.cnf && echo "Can't find my.cnf file, exit..." && exit 1
+}
